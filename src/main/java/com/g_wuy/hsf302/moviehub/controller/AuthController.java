e0b6d6fcb9cc16e5cdb12ae9b502684ac28dd40f
Duc Minh <nguyenphuocducminh06@gmail.com>
2025-11-06 22:39:40 +0700

    D.Minh/remove-java-security

--- src/main/java/com/g_wuy/hsf302/moviehub/controller/AuthController.java
+++ src/main/java/com/g_wuy/hsf302/moviehub/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.g_wuy.hsf302.moviehub.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
